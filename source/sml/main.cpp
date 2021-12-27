858a32797313d2f1f1cba9b2649f2a22ece5a5fc
GrandChris <christiansteinbrecher@gmx.at>
2021-12-27 10:45:44 +0100

    Folder Structure

---
+++ source/sml/main.cpp
@@ -0,0 +1,15 @@
+///
+/// @file:   main.cpp
+/// @author: GrandChris
+/// @date:   2021-12-27
+/// @brief:  Program entry point
+///
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
